32c6149d40a3ee8556461081ccfdfe283a09a68e
Cypress Frankenfeld <cfrankenfeld@onshape.com>
2017-01-23 11:02:07 -0500

    Disable scrolling upon button press

--- app/src/main/java/com/cypressf/myapplication/MainActivity.java
+++ app/src/main/java/com/cypressf/myapplication/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,12 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SimplePagerAdapter pagerAdapter = new SimplePagerAdapter(getSupportFragmentManager());
-        final ViewPager pager = (ViewPager) findViewById(R.id.simple_pager);
+        final ScrollBlockingViewPager pager = (ScrollBlockingViewPager) findViewById(R.id.simple_pager);
         pager.setAdapter(pagerAdapter);
         pager.setCurrentItem(0);
 
         final Button button1 = (Button) findViewById(R.id.button1);
         final Button button2 = (Button) findViewById(R.id.button2);
+        final ToggleButton buttonBlockScrolling = (ToggleButton) findViewById(R.id.toggleButton);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 pager.setCurrentItem(1);
+            }
+        });
+
+        buttonBlockScrolling.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                pager.setPagingEnabled(!isChecked);
             }
         });
     }
